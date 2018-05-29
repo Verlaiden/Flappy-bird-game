e6e3670d955f7b2f19179581ab1fe310e2b360d8
Kamil Hanysz <verlaiden.pl@gmail.com>
2018-05-29 03:46:38 +0200

    1

---
+++ game.js
@@ -0,0 +1,36 @@
+
+var bird;
+var pipes = [];
+
+function setup() {
+    createCanvas(400, 600);
+    bird = new Bird();
+    pipes.push(new Pipe());
+}
+function draw() {
+    background(0);
+    bird.update();
+    bird.show();
+
+    if (frameCount % 40 == 0) {
+        pipes.push(new Pipe());
+    }
+
+    for (var i = pipes.length-1; i >= 0; i--) {
+        pipes[i].show();
+        pipes[i].update();
+
+        if (pipes[i].hits(bird)){
+            
+        }
+
+        if (pipes[i].offscreen()){
+            pipes.splice(i, 1);
+        }
+    }
+}
+function keyPressed() {
+    if (key == ' ') {
+        bird.up();
+    }
+}
